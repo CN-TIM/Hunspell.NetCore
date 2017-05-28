5d184c8e417e526d8cd299eea7f98eb7692989b3
Rian Stockbower <rstockbower@gmail.com>
2017-05-28 09:55:01 -0400

    Rearrange folder structure a bit, get rid of PCL and net35 build targets

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,13 +1,5 @@
 ﻿using System.Reflection;
 
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#elif RELEASE
-[assembly: AssemblyConfiguration("Release")]
-#else
-[assembly: AssemblyConfiguration("Unknown")]
-#endif
-
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Hunspell.NetCore")]
 [assembly: AssemblyCopyright("")]
